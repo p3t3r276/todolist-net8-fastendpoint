a73e956ce59cb928a4125fb32d4761c93858eaff
khoale2706 <p3t3r276@gmail.com>
2025-06-25 17:17:04 +0700

    Setup Identity Dbcontext

---
+++ src/Persistence/EF/DbContexts/FastTodoIdentity/FastTodoIdentity.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FastTodo.Persistence.EF;
+
+public sealed class FastTodoIdentity : IdentityDbContext<AppUser>
+{
+    public FastTodoIdentity(DbContextOptions<FastTodoIdentity> options)
+        : base(options)
+    {
+    }
+}
+
+public class AppUser : IdentityUser
+{
+    // Add customisations here later
+}
