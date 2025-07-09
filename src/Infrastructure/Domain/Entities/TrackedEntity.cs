658f3fb09996807132ba1afbe833c21119b34e59
p3t3r276 <p3t3r276@gmail.com>
2025-07-10 00:36:20 +0700

    Allow date default entity to null

--- src/Infrastructure/Domain/Entities/TrackedEntity.cs
+++ src/Infrastructure/Domain/Entities/TrackedEntity.cs
@@ -4,9 +4,9 @@ public class TrackedEntity
 {
     public string CreatedBy { get; set; } = string.Empty;
 
-    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset? CreatedAt { get; set; }
 
     public string ModifiedBy { get; set; } = string.Empty;
 
-    public DateTimeOffset ModifiedAt { get; set; }
+    public DateTimeOffset? ModifiedAt { get; set; }
 }
